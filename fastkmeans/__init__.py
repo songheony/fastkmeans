262881125c040135c0c173e77d150dd4a0cfe212
songheony <songheony@gmail.com>
2025-05-09 04:21:52 +0900

    Update init.py

--- fastkmeans/__init__.py
+++ fastkmeans/__init__.py
@@ -1,4 +1,5 @@
 from .kmeans import FastKMeans
+from .launcher import distributed_fit, distributed_predict
 
-__all__ = ["FastKMeans"]
+__all__ = ["FastKMeans", "distributed_fit", "distributed_predict"]
 __version__ = "0.4.0"
